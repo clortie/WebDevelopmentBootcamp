82dfc9d1db1c5a4348e3b463a997a750beea5fe6
clortie <clortie.26@gmail.com>
2017-06-05 19:49:57 -0400

    updated todo list app

--- JavaScript/Todo List/list.js
+++ JavaScript/Todo List/list.js
@@ -1,11 +1,32 @@
 var list = [];
 var command = "";
+
 while(command!=="quit"){
 	if(command==="new"){
 		list.push(prompt("Enter new todo:"));
+		console.log(list[list.length-1]+" added to todos");
 	}
 	else if(command==="list"){
-		console.log(list);
+		if(list.length>0){
+			console.log("**********");
+			list.forEach(function(item,index){
+				console.log(index+": "+item);
+			});
+			console.log("**********");
+		}
+		else{
+			console.log("Nothing in todo list.");
+		}
+	}
+	else if(command==="delete"){
+		var item = Number(prompt("Enter index of todo to delete:"));
+		if(item>=0&&item<list.length){
+			list.splice(item,1);
+			console.log("Index "+item+" successfully removed.");
+		}
+		else{
+			console.log("Invalid index.");
+		}
 	}
 	command=prompt("What would you like to do?");
 }
