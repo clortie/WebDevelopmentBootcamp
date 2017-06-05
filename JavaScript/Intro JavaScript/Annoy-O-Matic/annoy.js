c65903fa0e46e2750637387b2930a0b97b46da1f
clortie <clortie.26@gmail.com>
2017-06-05 11:37:42 -0400

    Annoy-O-Matic exercise

---
+++ JavaScript/Intro JavaScript/Annoy-O-Matic/annoy.js
@@ -0,0 +1,6 @@
+//ask user are we there yet
+var response="";
+while(response.toLowerCase()!=="yes"&&response.toLowerCase()!=="yeah"){
+	response=prompt("Are we there yet?");
+}
+alert("Yay, we finally made it!");
