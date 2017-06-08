56bf9710d4628f4b2d87eb1f09c95d42f86b80f0
clortie <clortie.26@gmail.com>
2017-06-08 15:30:16 -0400

    DOM selector exercise

---
+++ DOM/SelectorExercise/selectorex.js
@@ -0,0 +1,8 @@
+var firstP = document.getElementsByClassName("special")[0];
+console.dir(firstP);
+firstP = document.querySelector("#first");
+console.dir(firstP);
+firstP = document.querySelector("body p");
+console.dir(firstP);
+firstP = document.querySelectorAll(".special")[0];
+console.dir(firstP);
