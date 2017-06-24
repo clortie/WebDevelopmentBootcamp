b8738c817d3a8b4bdb11f5eb30c0efdcc42becbe
Calvin Lortie <clortie.26@gmail.com>
2017-06-24 18:30:44 +0000

    yelpcamp v1 setup

---
+++ YelpCamp/v1/app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [{name: "Salmon Creek", image: "https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc.jpg"},
+        {name: "Death Valley", image: "https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc.jpg"},
+        {name: "Lover's Cliff", image: "https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc.jpg"}
+    ];
+    res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server has started");
+});
